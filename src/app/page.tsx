bde34435c901e28c471b93f621067acd9df0bd2b
huanshenyi <txy1226052@gmail.com>
2024-06-19 22:14:48 +0900

    add: first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Textarea } from "@/components/Textarea";
 
 export default function Home() {
   return (
-    <div>home
-      <Button variant={"ghost"} type="submit">Click me</Button>
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4">
+        <h1 className="text-center text-2xl font-bold">Create App</h1>
+        <Input type="text" placeholder="App Name" name="name" />
+        <Textarea placeholder="description" name="Description" />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 }
