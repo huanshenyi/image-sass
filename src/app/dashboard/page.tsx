3706148f02583205f3b96d5da3e30a86be296805
huanshenyi <txy1226052@gmail.com>
2024-08-18 15:55:37 +0900

    init upper

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,46 @@
-import { createContext, serverCaller } from "@/utils/trpc";
+"use client";
 
-export default async function Dashboard() {
-  const context = await createContext();
-  const data = await serverCaller(context).hello();
+import { Uppy } from "@uppy/core";
+import AWSS3 from "@uppy/aws-s3";
+import { useState } from "react";
+import { useUppyState } from "./useUppyStatus";
+
+export default function Dashboard() {
+  const [uppy] = useState(() => {
+    const uppy = new Uppy();
+    uppy.use(AWSS3, {
+      shouldUseMultipart: false,
+      getUploadParameters() {
+        return {
+          url: "", //pre signed url
+        };
+      },
+    });
+    return uppy;
+  });
+
+  const files = useUppyState(uppy, (s) => Object.values(s.files));
+
   return (
     <div className="h-screen flex justify-center items-center">
-      Dashboard {data.hello}
+      <input
+        type="file"
+        onChange={(e) => {
+          if (e.target.files) {
+            Array.from(e.target.files).forEach((file) => {
+              uppy.addFile({
+                name: file.name,
+                data: file,
+              });
+            });
+          }
+        }}
+        multiple
+      ></input>
+      {files.map((file) => {
+        const url = URL.createObjectURL(file.data);
+        return <img src={url} key={file.id}></img>;
+      })}
     </div>
   );
 }
